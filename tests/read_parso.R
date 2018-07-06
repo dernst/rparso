33bd5e791fda05a93a40a974eba224222532e78f
Dominik Ernst <github@dernst.org>
2018-07-06 15:56:50 +0200

    changes

---
+++ tests/read_parso.R
@@ -0,0 +1,26 @@
+
+if(FALSE) {
+
+    fn = system.file("examples", "iris.sas7bdat", package="haven")
+    x = read_parso(fn)
+
+    x = read_parso(fn, nrow=10)
+
+    x = read_parso(fn, nrow=10, skip=10)
+
+    x = read_parso(fn, nrow=160, skip=10)
+
+    x = read_parso(fn, select=c("Sepal_Length", "Petal_Length"))
+    x = read_parso(fn, select=c("Petal_Length", "Sepal_Length"))
+
+
+    x = read_parso(path.expand("~/mnt/bvs/Daten/DBABZUG20180627/tsch.sas7bdat"), nrow=100, encoding="UTF-8")
+
+
+    print(system.time({
+        x = read_parso(path.expand("~/mnt/bvs/Daten/DBABZUG20180627/tsch.sas7bdat"), encoding="UTF-8")
+    }))
+
+
+}
+
